0a754f90fe5a7a86726e9c11f89b4a12e4f4088d
isaacBejarano <isaacbejaranopro@gmail.com>
2020-11-24 03:27:24 +0100

    list of heroes + details on selected

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../interfaces/hero';
+import { HEROES } from '../data/mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss'],
+  /* file vs. hardcoded */
+  // template: '<b>hardcoded Template</b>',
+  // styles: ['b { color:red; }'],
+})
+export class HeroesComponent implements OnInit {
+  selected: Hero; // show html *ngIf selected populated
+
+  heroes: Hero[] = HEROES;
+
+  constructor() {}
+
+  // hooks
+  ngOnInit(): void {
+    // initialization (of props) logic
+  }
+
+  // methods
+  onSelect(hero: Hero): void {
+    this.selected = hero;
+  }
+}
